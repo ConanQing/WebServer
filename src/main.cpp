b28f968565b6c2fd32551bedf63768e00c2000a3
conanqing <2435562354@qq.com>
2026-09-17 13:50:42 +0800

    add epoll

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include<sys/socket.h>//Socket 相关函数
 #include<netinet/in.h>//定义互联网地址（IPv4）的数据结构和字节序转换函数，专门用来处理 IP 地址和端口
 #include<arpa/inet.h>//提供 IP 地址格式转换的函数
-
+#include<sys/epoll.h>
 #include<iostream>
 #include<cstring>
 #include<unistd.h>//提供文件描述符操作、进程控制等基础系统函数
 #include<string>
 #include<sstream>
+#include<fstream>
 
 int main()
 {
@@ -46,69 +47,103 @@ int main()
     }
     std::cout<<"server is listening on port 8080\n";
 
+    //epoll
+    int epoll_fd = epoll_create1(0);
+
+    if(epoll_fd == -1)
+    {
+        perror("epoll_create1");
+        close(listen_fd);
+        return 1;
+    }
+
+    //准备一个epoll_event
+    epoll_event event{};
+    event.events = EPOLLIN;//关心这个 fd 有没有“可读事件”
+    event.data.fd = listen_fd;//这个事件对应的是 listen_fd
+
+    //EPOLL_CTL_ADD:把一个 fd 加入 epoll 的监视名单
+    if(epoll_ctl(epoll_fd,EPOLL_CTL_ADD,listen_fd,&event) == -1)
+    {
+        perror("epoll_ctl");
+        close(epoll_fd);
+        close(listen_fd);
+        return 1;
+    }
+    //准备一个数组
+    epoll_event events[1024];
+
     //接受客户端请求
     while(true)
     {
-        //装客户端的地址，还有长度
-        sockaddr_in client_addr{};
-        socklen_t client_addr_len = sizeof(client_addr);
+        int ready = epoll_wait(epoll_fd,events,1024,-1);
 
-        int client_fd = accept(listen_fd,
-        reinterpret_cast<sockaddr*>(&client_addr),
-        &client_addr_len);
-
-        if(client_fd == -1)
+        for(int i = 0;i<ready;++i)
         {
-            perror("accept");
-            close(listen_fd);
-            return 1;
+            int temp_fd = events[i].data.fd;
+            //有新客户端来了
+            if(temp_fd == listen_fd)
+            {
+                sockaddr_in client_addr{};
+                socklen_t client_addr_len = sizeof(client_addr);
+
+                int client_fd = accept(listen_fd,
+                    (sockaddr*)&client_addr,
+                    &client_addr_len);
+                if(client_fd = -1)
+                {
+                    perror("accept");
+                    continue;
+                }
+
+                std::cout<<"new client: "<<client_fd<<"\n";
+                // 把新客户端 fd 注册进 epoll，监听它的可读事件
+                epoll_event client_event{};
+                client_event.events = EPOLLIN;
+                client_event.data.fd = client_fd;
+                if(epoll_ctl(epoll_fd, 
+                    EPOLL_CTL_ADD, 
+                    client_fd, 
+                    &client_event) == -1)
+                {
+                    perror("epoll_ctl");
+                    close(client_fd);
+                    continue;
+                }
+
+            }
+            //已连接客户
+            else
+            {
+                char buffer[4096] = {0};
+
+                int n = recv(temp_fd,buffer,sizeof(buffer)-1,0);
+                //客户端断开
+                if(n <= 0)
+                {
+                    close(temp_fd);
+                    epoll_ctl(epoll_fd,EPOLL_CTL_DEL,temp_fd,nullptr);
+                    std::cout<<"client disconnected: "<<temp_fd<<"\n";
+                    continue;
+                }
+
+                buffer[n] = '\0';
+                std::string body = "Hello from ConanWebServer!";
+                std::string response;
+                response += "HTTP/1.1 200 OK\r\n";
+                response += "Content-Type: text/plain\r\n";
+                response += "Content-Length: ";
+                response += std::to_string(body.size());
+                response += "\r\n";
+                response += "\r\n";
+                response += body;
+
+                send(temp_fd,response.c_str(),response.size(),0);
+            }
         }
-
-        std::cout<<"new client connected,client_fd = " <<client_fd<<"\n";
-
-        char buffer[4096] = {0};
-
-        int n = recv(client_fd,buffer,sizeof(buffer) - 1,0);
-        if(n == -1)
-        {
-            perror("recv");
-            close(client_fd);
-            close(listen_fd);
-            return 1;
-        }
-        else if(n == 0)
-        {
-            std::cout<<"client disconnected\n";
-            close(client_fd);
-            close(listen_fd);
-            return 0;
-        }
-
-        std::cout<<"received:\n";
-        std::cout<< buffer <<"\n";
-
-        std::istringstream iss(buffer);
-        std::string method;
-        std::string path;
-        std::string version;
-
-        iss >> method >>path >> version;
-
-        std::string body = "Hello Conan!";
-
-        std::string response;
-
-        response += "HTTP/1.1 200 OK\r\n";
-        response += "Content-Type: text/plain\r\n";
-        response += "Content-Length: ";
-        response += std::to_string(body.size());
-        response += "\r\n";
-        response += "\r\n";
-        response += body;
-
-        send(client_fd,response.c_str(),response.size(),0);
-    }
-
-
+    }       
+    close(epoll_fd);
+    close(listen_fd);
+    
     return 0;
 }
